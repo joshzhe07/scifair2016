c4342c195b7c185bdce4d10968a9c6ea4648bb6f
Josh Zhe <joshuazhe@gmail.com>
2017-01-06 04:33:55 +0000

    v1 with fake focallen

--- testdistance.py
+++ testdistance.py
@@ -28,6 +28,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
         blur = cv2.blur(image, (3,3))
 
+
         #hsv to complicate things, or stick with BGR
         #hsv = cv2.cvtColor(blur,cv2.COLOR_BGR2HSV)
         #thresh = cv2.inRange(hsv,np.array((0, 200, 200)), np.array((20, 255, 255)))
@@ -35,8 +36,8 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         #lower = np.array([27,10,143],dtype="uint8")
         #upper = np.array([107,49,203], dtype="uint8")
 
-        lower = np.array([27,10,123],dtype="uint8")
-        upper = np.array([107,49,223], dtype="uint8")
+        lower = np.array([17,1,123],dtype="uint8")
+        upper = np.array([117,59,223], dtype="uint8")
 
         #lower = np.array([65,47,200],dtype="uint8")
         #upper = np.array([125,107,260], dtype="uint8")
@@ -47,6 +48,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
         # upper = np.array([25, 25, 250],dtype="uint8")
         thresh = cv2.inRange(blur, lower, upper)
+        thresh = cv2.GaussianBlur(thresh, (3, 3), 0)
         thresh2 = thresh.copy()
 
         # find contours in the threshold image
@@ -55,22 +57,15 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         # finding contour with maximum area and store it as best_cnt
         max_area = 0
         best_cnt = 1
-        for cnt in contours:
-                area = cv2.contourArea(cnt)
-                #print "cnt",cnt
-                if area > max_area:
-                        max_area = area
-                        best_cnt = cnt
-
-        # finding centroids of best_cnt and draw a circle there
-        M = cv2.moments(best_cnt)
-        cx,cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
-        #if best_cnt>1:
-        cv2.circle(blur,(cx,cy),10,(0,255,0),-1)
-        #cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-        # show the frame
+        focallen=1.23
+        if len(contours) > 0:
+	          cnt = sorted(contours, key = cv2.contourArea, reverse = True)[0]
+                  rect = np.int32(cv2.boxPoints(cv2.minAreaRect(cnt)))
+                  cv2.drawContours(blur, [rect], -1, (0, 255, 0), 2)
+                  cv2.putText(blur,"%dpx %.2fft" % (rect[1][0],rect[1][0]/focallen), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255),2)
         cv2.imshow("Frame", blur)
         cv2.imshow('thresh',thresh2)
+        time.sleep(0.025)
         #print "best_cnt",best_cnt,"area",max_area
         key = cv2.waitKey(1) & 0xFF
 
